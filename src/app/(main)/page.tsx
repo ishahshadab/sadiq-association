216ce3b5a235d379db882801b0b96a03a4f9774b
Shadab shah <shadabshah2508@gmail.com>
2026-06-07 04:16:33 +0530

    Fix page.tsx - remove quotes

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,4 @@
-"import Hero from '@/components/home/Hero';
+import Hero from '@/components/home/Hero';
 import AboutSection from '@/components/home/AboutSection';
 import WhyChooseUs from '@/components/home/WhyChooseUs';
 import ServicesOverview from '@/components/home/ServicesOverview';
@@ -25,4 +25,3 @@ export default function HomePage() {
     </>
   );
 }
-"
